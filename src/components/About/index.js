3bea0018359aeafc37977fb022ab10ab7c87976a
David Cuthell <dcc1447@gmail.com>
2018-07-19 15:59:32 -0700

    added content for about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import { Container, Jumbotron, Row, Col } from 'reactstrap';
 import './styles.css';
 
 class About extends Component {
 render() {
   return(
-      <main className='wrapper'>
-        <div className="aboutWrapper">
-          <section>
-
-            <div className="lineStyle5">Brick and Motor is a wood fired pizza food cart located at Billy Ray's Dive on NE MLK Jr. Blvd and Thompson St. Our dough is hand mixed daily using a 12 hour ferment to increase flavor. We make everything to order using the freshest local ingredients. Come grab a pizza and sit down at Billy Ray's heated patio or have it delivered through Amazon Restaurants.</div>
-            <div className="lineStyle6">Serving Satisfaction from 4 PM to LATE.</div>
-
-          </section>
-        </div>
-      </main>
+      <Container className="About">
+				<Row>
+					<Col>
+						<Jumbotron>
+							<h1 className="display-3">Hot Dogs</h1>
+			        <p className="lead">Life can be complicated. Hot dogs are simple. They come in different styles, and they taste great with a beer and a side of fries.</p>
+			        <hr className="my-1" />
+			        <p>Come by for a hotdog today.</p>
+						</Jumbotron>
+					</Col>
+				</Row>
+			</Container>
     );
   }
 }
