6a9359e2c5d2784efe8ef47672b9e642cb7f1a33
David Cuthell <dcc1447@gmail.com>
2018-07-19 16:15:31 -0700

    changed content for contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,36 +1,33 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from 'reactstrap';
 import "./styles.css";
 
 class Contact extends Component {
   render() {
     return (
-      <main className="wrapper">
-        <section>
-          <div className="lineStyle6">Call [ 971 ] 988 - 6575 </div>
-          <hr />
-          <div className="lineStyle6">
-            <a
-              href="https://www.amazon.com/restaurants/brick-and-motor-portland/d/B079J6MDKB?ref_=amzrst_b_B079J6MDKB_23"
-              alt="link to amazon delivery"
-            >
-              Amazon Delivery
-            </a>
-          </div>
-          <hr />
-          <div className="mapBox">
-            <iframe
-              height="400px"
-              width="100%"
-              frameborder="0"
-              styles="border:0"
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDozY9mgvE7tAZjIUB2d51Ss5EbmKBeKRM
-                &q=Brick+and+Motor+Pizza,Portland+OR
-                &zoom=14"
-              allowfullscreen>
-            </iframe>
-          </div>
-        </section>
-      </main>
+			<Container className= "Contact">
+				<Row>
+					<Col>
+						<h1>Call (503) 233-4616</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<div className="mapBox">
+	            <iframe
+	              height="400px"
+	              width="100%"
+	              frameborder="0"
+	              styles="border:0"
+	              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDozY9mgvE7tAZjIUB2d51Ss5EbmKBeKRM
+	                &q=Zach's+Shack,Portland+OR
+	                &zoom=14"
+	              allowfullscreen>
+	            </iframe>
+	          </div>
+					</Col>
+				</Row>
+			</Container>
     );
   }
 }
