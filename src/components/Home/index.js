efe90749448de4cf8a1506386fc3e0819b214ba7
David Cuthell <dcc1447@gmail.com>
2018-07-20 21:35:35 -0700

    change home content, remove old style, add min height to router

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import PrimeNow from '../../img/prime.jpg';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import './styles.css';
 
 
 class Home extends Component {
   render() {
     return (
-      <main className='wrapper'>
-        <section className='section parallax bg1'>
-          <div className="lineStyle1">Zach's</div>
-          <div className="lineStyle2">Shack</div>
-          <div className="lineStyle1">MuthaFuckin</div>
-          <div className="lineStyle2">Hot Dawgs</div>
-        </section>
-        <section className='section static'>
-          <div className="primeTime">
-            <br/>
-            <p className="lineStyle2">Order Your Dawgs for Takeout</p>
-            <a href="https://www.amazon.com/restaurants/brick-and-motor-portland/d/B079J6MDKB?ref_=amzrst_b_B079J6MDKB_20" target="_blank" rel="noopener noreferrer"><img src={PrimeNow} alt='amazon restaurant delivery' id="primeLogo"></img></a>
-          </div>
-        </section>
-        <section className='section parallax bg2'>
-        </section>
-      </main>
+			<Container className="Home">
+				<Row>
+					<Col>
+						<Jumbotron>
+							<h1 className="display-3">Zach's Shack</h1>
+							<h1 className="display-4">Hot Dogs</h1>
+          		<p className="lead">Come check us out</p>
+						</Jumbotron>
+					</Col>
+				</Row>
+			</Container>
     );
   }
 }
