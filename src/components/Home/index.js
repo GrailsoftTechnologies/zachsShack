6a3fa7c4efa391de902d17df8e14626b36cde8a3
David Cuthell <dcc1447@gmail.com>
2018-07-22 13:26:16 -0700

    change font for home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,8 +10,8 @@ class Home extends Component {
 				<Row>
 					<Col>
 						<Jumbotron>
-							<h1 className="display-3">Zach's Shack</h1>
-							<h1 className="display-4">Hot Dogs</h1>
+							<h1 className="display-3">ZACHS SHACK</h1>
+							<h1 className="display-4">HOT DOGS</h1>
 							<Button href={"#/Contact"} color="secondary">Find us</Button>
 						</Jumbotron>
 					</Col>
@@ -19,8 +19,8 @@ class Home extends Component {
 				<Row>
 					<Col>
 						<Jumbotron>
-							<h1 className="display-3">Hot Dogs</h1>
-							<h1 className="display-4">Delivered</h1>
+							<h1 className="display-3">HOT DOGS</h1>
+							<h1 className="display-4">DELIVERED</h1>
 							<Button href={"https://postmates.com/merchant/zachs-shack-portland"} color="success">Order Here</Button>
 						</Jumbotron>
 					</Col>
