d956e69da42394c0eab2237f905c06a2c3f4f582
David Cuthell <dcc1447@gmail.com>
2018-08-06 14:16:53 -0700

    update styling for altnav

--- src/components/AltNavBar/index.js
+++ src/components/AltNavBar/index.js
@@ -6,42 +6,48 @@ class AltNavBar extends Component {
   constructor(props) {
   	super(props);
 	}
+
+
   render() {
     return (
-      <Container className="AltNavBar">
-				<Row>
-					<Col xs='3'>
-						<Button href={"https://postmates.com/merchant/zachs-shack-portland"} color="success">Order Here</Button>
-					</Col>
-					<Col xs='9' id="socialIcons">
-						<span className="footer-icon icon fa fa-yelp desktop-only"></span>
-						<span className="footer-icon icon fa fa-facebook-square desktop-only"></span>
-						<span className="footer-icon icon fa fa-instagram desktop-only"></span>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<Container>
-							<Row>
-								<Col>
-									<h1>Zach's Shack</h1>
-								</Col>
-							</Row>
-						</Container>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<a href="/#/menu">Menu</a>
-					</Col>
-					<Col>
-						<a href="/#/about">About</a>
-					</Col>
-					<Col>
-						<a href="/#/contact">Contact</a>
-					</Col>
-				</Row>
-			</Container>
+      <div className="AltNavBar">
+				<Container>
+					<Row>
+						<Col xs='3'>
+							<Button href={"https://postmates.com/merchant/zachs-shack-portland"} color="secondary">Order Here</Button>
+						</Col>
+						<Col xs='9' id="socialIcons">
+							<span className="footer-icon icon fa fa-yelp desktop-only"></span>
+							<span className="footer-icon icon fa fa-facebook-square desktop-only"></span>
+							<span className="footer-icon icon fa fa-instagram desktop-only"></span>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Container>
+								<Row>
+									<Col>
+										<a href="/#"><h1>Zach's Shack</h1></a>
+									</Col>
+								</Row>
+							</Container>
+						</Col>
+					</Row>
+					<Row>
+						<Col md="3"></Col>
+						<Col>
+							<a href="/#/menu">Menu</a>
+						</Col>
+						<Col>
+							<a href="/#/about">About</a>
+						</Col>
+						<Col>
+							<a href="/#/contact">Contact</a>
+						</Col>
+						<Col md="3"></Col>
+					</Row>
+				</Container>
+			</div>
     );
   }
 }
