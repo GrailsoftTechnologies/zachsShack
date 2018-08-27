a5d5bcf1a0d03b18aec93e1432ac34f71e318957
David Cuthell <dcc1447@gmail.com>
2018-08-26 19:41:36 -0700

    finish content for menu

--- src/components/AltMenu/index.js
+++ src/components/AltMenu/index.js
@@ -6,6 +6,31 @@ class AltMenu extends Component {
 render() {
  return(
   <Container className="AltMenu">
+    <Container className="MenuSection">
+      <Row>
+        <Col className="MenuHeader">
+          <h1>
+            Signature Dogs
+          </h1>
+          <hr className="my-2"></hr>
+          <i>
+            All signature dogs are prepared with Red Hot Chicago All Beef Dogs in natural casing.
+          </i>
+          <h4>
+            Substitute the following for no additional charge:
+          </h4>
+          <i>
+            Tofu Pup or Turkey Dog
+          </i>
+          <h4>
+            Substitute any of the following for $1.50:
+          </h4>
+          <i>
+            Red Hot Spicy Sausage, Cheese Filled Sausage, Linguisa Pork Sausage, or Field Roast Veggie Sausage
+          </i>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
 			<Row>
 				<Col className="dogType">
 					<h1>
@@ -149,6 +174,187 @@ render() {
 				</Col>
 			</Row>
 		</Container>
+    <Container className="MenuSection">
+      <Row>
+        <Col className="MenuHeader">
+          <h1>
+            Build Your Own
+          </h1>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h3>
+						Free Toppings
+					</h3>
+					<p>
+						Yellow Mustard, Brown Mustard, Ketchup, Onions, Mayo, Celery Salt, Neon Green Relish, Red Relish, Dill Relish, Sweet Green Relish
+					</p>
+				</Col>
+			</Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h3>
+						75 cent Toppings
+					</h3>
+					<p>
+						Tomato Slices, Pickle Slices, Pickle Spears, Black Olives, Jalapenos, Pepperoncini, Salsa, Cucumbers, Sour Cream, Sport Peppers, Carrots, Side of Ranch or Marinara
+					</p>
+				</Col>
+			</Row>
+      <hr className="my-2"></hr>
+      <Row>
+        <Col>
+          <h3>
+            $1.50 Toppings
+          </h3>
+          <p>
+            Cream Cheese, Cheddar CHeese, Blue Cheese, Chili, Veggie Chili, Cole Slaw, Sauerkraut
+          </p>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
+      <Row>
+        <Col>
+          <h3>
+            Gluten Free Buns $1.50
+          </h3>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="MenuSection">
+      <Row>
+        <Col className="MenuHeader">
+          <h1>
+            Fries
+          </h1>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
+      <Row>
+        <Col>
+          <h3>
+            Small $3.50
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>
+            Regular $5.50
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>
+            Cheddar $6.50
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>
+            Cheddar and Jalapeno $7
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>
+            Chili and Cheddar $8
+          </h3>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="MenuSection">
+      <Row>
+        <Col className="MenuHeader">
+          <h1>
+            Extras
+          </h1>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h1>
+						Mozzarella Sticks (5) $7.50
+					</h1>
+					<p>
+						served with marinara sauce
+					</p>
+				</Col>
+			</Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h1>
+						Jalapeno Poppers (5) $7.50
+					</h1>
+					<p>
+						stuffed with cream cheese and served with sweet chili Thai sauce
+					</p>
+				</Col>
+			</Row>
+      <hr className="my-2"></hr>
+      <Row>
+        <Col>
+          <h1>
+            Chips $1.50
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="MenuSection">
+      <Row>
+        <Col className="MenuHeader">
+          <h1>
+            Drinks
+          </h1>
+        </Col>
+      </Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h1>
+						Soda (free refills) $2.50
+					</h1>
+					<p>
+						Coke
+					</p>
+          <p>
+						Diet Coke
+					</p>
+          <p>
+						Sprite
+					</p>
+          <p>
+						Ginger Ale
+					</p>
+          <p>
+						Orange
+					</p>
+          <p>
+						Ice Tea
+					</p>
+          <p>
+						Lemonade $3(no refill)
+					</p>
+				</Col>
+			</Row>
+      <hr className="my-2"></hr>
+      <Row>
+				<Col>
+					<h1>
+            Local Craft Beer, Wine, and Liquor Available
+					</h1>
+				</Col>
+			</Row>
+    </Container>
+  </Container>
   );
  }
 }
