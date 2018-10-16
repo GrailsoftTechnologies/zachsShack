87d1110392c8377acf59b50f231572ae113c913d
David Cuthell <dcc1447@gmail.com>
2018-10-16 15:42:55 -0700

    import serviceworker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'reactstrap';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
