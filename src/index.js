d76a0e5cdadc5e6cf6d1a0821ae6cec7e1f49a8a
David Cuthell <dcc1447@gmail.com>
2018-10-16 15:24:11 -0700

    switch to browserrouter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'reactstrap';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <HashRouter basename={ process.env.PUBLIC_URL }>
+  <BrowserRouter basename={ process.env.PUBLIC_URL }>
     <App />
-  </HashRouter>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
