a84d7d3c78de610fcd3f82672aaa71a1063ce785
David Cuthell <dcc1447@gmail.com>
2018-11-01 22:11:44 -0700

    make additional style changes to navbar and router components

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,17 +8,15 @@ render() {
       <Container className="About">
 				<Row>
 					<Col>
-						<Jumbotron className="aboutText">
-							<h1 className="display-3">Zach's Shack Hot Dogs...</h1>
-			        <h4 className="lead">
-								...are 100% beef dogs with a natural casing.
-                It's the casing and the steaming that gives the dog the "snap". 
-                You may choose to substitute a tofu dog or veggie sausage (will take 4 minutes longer to prepare) or a turkey dog (8 minutes) at no extra charge.
-                We are open until 3:00 am every night.
-                Thanks for stopping by!
-							</h4>
-							<h4>-Zach</h4>
-						</Jumbotron>
+						<h1>Zach's Shack Hot Dogs...</h1>
+		        <h4>
+							...are 100% beef dogs with a natural casing.
+              It's the casing and the steaming that gives the dog the "snap".
+              You may choose to substitute a tofu dog or veggie sausage (will take 4 minutes longer to prepare) or a turkey dog (8 minutes) at no extra charge.
+              We are open until 3:00 am every night.
+              Thanks for stopping by!
+						</h4>
+						<h4>-Zach</h4>
 					</Col>
 				</Row>
 			</Container>
