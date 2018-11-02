b5a0bdd404599613d82d37db040b4f78bee8e97d
David Cuthell <dcc1447@gmail.com>
2018-11-01 21:06:47 -0700

    make styling changes to navbar, add header sizes to router.css

--- src/components/AltNavBar/index.js
+++ src/components/AltNavBar/index.js
@@ -5,44 +5,40 @@ import './styles.css';
 class AltNavBar extends Component {
   render() {
     return (
-      <div className="AltNavBar">
-				<Container>
-					<Row>
-						<Col xs='5'>
-							<Button href={"https://postmates.com/merchant/zachs-shack-portland"} color="secondary">Order Here</Button>
-						</Col>
-						<Col xs='7' className="socialIcons">
-							<a href="https://www.yelp.com/biz/zachs-shack-portland"><span className="footer-icon icon fa fa-yelp desktop-only"></span></a>
-							<a href="https://www.facebook.com/ZachsShackHotdogs/"><span className="footer-icon icon fa fa-facebook-square desktop-only"></span></a>
-							<a href="https://www.instagram.com/explore/locations/227503863/zachs-shack/"><span className="footer-icon icon fa fa-instagram desktop-only"></span></a>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<Container>
-								<Row>
-									<Col>
-										<a href="/"><h1>Zach's Shack</h1></a>
-									</Col>
-								</Row>
-							</Container>
-						</Col>
-					</Row>
-					<Row>
-						<Col md="3"></Col>
-						<Col>
-							<a href="/menu">Menu</a>
-						</Col>
-						<Col>
-							<a href="/about">About</a>
-						</Col>
-						<Col>
-							<a href="/contact">Contact</a>
-						</Col>
-						<Col md="3"></Col>
-					</Row>
-				</Container>
-			</div>
+			<Container className="AltNavBar">
+				<Row className="justify-content-between">
+					<Col xs='3'>
+						<Button href={"https://postmates.com/merchant/zachs-shack-portland"} color="secondary">Order Here</Button>
+					</Col>
+          <Col xs='3' className="socialIcons">
+						<a href="https://www.yelp.com/biz/zachs-shack-portland"><span className="footer-icon icon fa fa-yelp desktop-only"></span></a>
+						<a href="https://www.facebook.com/ZachsShackHotdogs/"><span className="footer-icon icon fa fa-facebook-square desktop-only"></span></a>
+						<a href="https://www.instagram.com/explore/locations/227503863/zachs-shack/"><span className="footer-icon icon fa fa-instagram desktop-only"></span></a>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Container>
+							<Row>
+								<Col>
+									<a href="/"><h1>Zach's Shack</h1></a>
+								</Col>
+							</Row>
+						</Container>
+					</Col>
+				</Row>
+				<Row className="justify-content-center">
+					<Col xs="3">
+						<a href="/menu"><h4>Menu</h4></a>
+					</Col>
+					<Col xs="3">
+						<a href="/about"><h4>About</h4></a>
+					</Col>
+					<Col xs="3">
+						<a href="/contact"><h4>Contact</h4></a>
+					</Col>
+				</Row>
+			</Container>
     );
   }
 }
