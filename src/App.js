a910553caa095231e3dadeb30642eb96b6c7667a
David Cuthell <dcc1447@gmail.com>
2018-11-02 12:13:27 -0700

    make height changes to footer and router, remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Router from './components/Router/index.js';
 import AltNavBar from './components/AltNavBar';
 import Footer from './components/Footer';
 import './App.css';
-import Background from './img/postmates.jpg'
+import Background from './img/background.jpg'
 import MobileBackground from './img/mobilebackground.jpg'
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <AltNavBar color="black" background="red" hovercolor="white" buttoncolor="warning"/>
         <Router color="white" background="red" hovercolor="black" backgroundImage={Background} mobileBackgroundImage={MobileBackground}/>
-        <Footer/>
+        <Footer color="white" background="black" hovercolor="grey"/>
       </div>
     );
   }
