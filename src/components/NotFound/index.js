e237c54a7fed79bcf396b2217530564f1247c96a
David Cuthell <dcc1447@gmail.com>
2019-02-23 17:23:42 -0700

    change notfound to pure component, change content

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './styles.css';
+import React from 'react';
+import { Container, Row, Col, Jumbotron } from 'reactstrap'
 
-class NotFound extends Component {
-  render() {
-    return (
-      <div className="page-container">
-        <div className="lineStyle1 title">404</div>
-        <div className="lineStyle2">404 Page here</div>
-        <center><Link to="/">Return to Home Page</Link></center>
-      </div>
-    );
-  }
+export default function NotFound(props) {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Jumbotron style={{color: 'inherit', backgroundColor: 'inherit'}}>
+            <h1>404</h1>
+            <h3>Uh oh, something went wrong</h3>
+            <a href="/"><h3>Return to home page</h3></a>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
-
-export default NotFound;
