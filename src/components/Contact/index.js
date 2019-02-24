b29b4c9bed49a8dd038324810948d9d733ff593e
David Cuthell <dcc1447@gmail.com>
2019-02-23 17:12:36 -0700

    change contact to pure component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from 'reactstrap';
-import "./styles.css";
-import GoogleMapFrame from '../GoogleMapFrame';
+import React from "react"
+import { Container, Row, Col } from 'reactstrap'
+import "./styles.css"
+import GoogleMapFrame from '../GoogleMapFrame'
 
-class Contact extends Component {
-  render() {
-    return (
-			<Container className= "Contact">
-				<Row>
-					<Col>
-            <a href="tel:+15032334616">
-              <h3>FOR CALL IN ORDERS</h3>
-              <h3>(503)-233-4616</h3>
-            </a>
-					</Col>
-				</Row>
-        <hr></hr>
-        <Row>
-					<Col>
-            <a href="mailto:contact@zachsshack.com">
-              <h3>FOR PARTIES OR EVENTS</h3>
-              <h3>CONTACT@ZACHSSHACK.COM</h3>
-            </a>
-					</Col>
-				</Row>
-        <hr></hr>
-				<Row>
-					<Col>
-						<GoogleMapFrame name="Zach's+Shack" location="Portland+OR"/>
-					</Col>
-				</Row>
-        <hr></hr>
-        <Row className="justify-content-center">
-          <Col xs='1'>
-						<a href="https://www.yelp.com/biz/zachs-shack-portland" target="_blank" rel="noreferrer noopener"><i className="fab fa-yelp icon" /></a>
-					</Col>
-          <Col xs='1'>
-            <a href="https://www.facebook.com/ZachsShackHotdogs/" target="_blank" rel="noreferrer noopener"><i className="fab fa-facebook icon" /></a>
-          </Col>
-          <Col xs='1'>
-            <a href="https://www.instagram.com/explore/locations/227503863/zachs-shack/" target="_blank" rel="noreferrer noopener"><i className="fab fa-instagram icon" /></a>
-          </Col>
-				</Row>
-			</Container>
-    );
-  }
+export default function Contact(props) {
+  return (
+    <Container className= "Contact">
+      <Row>
+        <Col>
+          <a href="tel:+15032334616">
+            <h3>FOR CALL IN ORDERS</h3>
+            <h3>(503)-233-4616</h3>
+          </a>
+        </Col>
+      </Row>
+      <hr></hr>
+      <Row>
+        <Col>
+          <a href="mailto:contact@zachsshack.com">
+            <h3>FOR PARTIES OR EVENTS</h3>
+            <h3>CONTACT@ZACHSSHACK.COM</h3>
+          </a>
+        </Col>
+      </Row>
+      <hr></hr>
+      <Row>
+        <Col>
+          <GoogleMapFrame name="Zach's+Shack" location="Portland+OR"/>
+        </Col>
+      </Row>
+      <hr></hr>
+      <Row className="justify-content-center">
+        <Col xs='1'>
+          <a href="https://www.yelp.com/biz/zachs-shack-portland" target="_blank" rel="noreferrer noopener"><i className="fab fa-yelp icon" /></a>
+        </Col>
+        <Col xs='1'>
+          <a href="https://www.facebook.com/ZachsShackHotdogs/" target="_blank" rel="noreferrer noopener"><i className="fab fa-facebook icon" /></a>
+        </Col>
+        <Col xs='1'>
+          <a href="https://www.instagram.com/explore/locations/227503863/zachs-shack/" target="_blank" rel="noreferrer noopener"><i className="fab fa-instagram icon" /></a>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
-
-export default Contact;
